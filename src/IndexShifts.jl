c1518a09bca7e4dc05f43978f4c4f2e63c84e2d3
gpapo <gpapo95@gmail.com>
2018-05-08 18:18:55 +0200

    Add documentation

--- src/IndexShifts.jl
+++ src/IndexShifts.jl
@@ -5,6 +5,31 @@ export @shiftindex
 using Compat
 using MacroTools
 
+"""
+
+    @shiftindex shift blk
+
+Shift the index inside square brackets/`getindex` of `shift` positions: `a[i]`
+becomes `a[i + shift]`. It can be applied even to a block of code.
+
+# Examples
+```julia-repl
+julia> x = [1, 2, 3];
+
+julia> @shiftindex 1 x[2]
+3
+
+julia> @macroexpand @shiftindex 1 begin
+           x[0] + x[2]
+           x[0:2]
+       end
+quote  # none, line 2:
+    x[0 + 1] + x[2 + 1] # none, line 3:
+    x[(0:2) + 1]
+end
+
+```
+"""
 macro shiftindex(shift, blk)
     return esc(MacroTools.postwalk(blk) do x
         if isa(x, Expr)
