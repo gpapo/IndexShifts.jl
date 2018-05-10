72d78d876d8c58f737322eda43c1852bc76da9fb
gpapo <gpapo95@gmail.com>
2018-05-11 01:06:29 +0200

    Add specialized functions to shift index inside a ref/getindex
    e.g. if index is an integer the shift is inlined, if it's a vector the
    shift is broadcasted etc.

--- src/IndexShifts.jl
+++ src/IndexShifts.jl
@@ -5,21 +5,26 @@ export @shiftindex
 using Compat
 using MacroTools
 
-_shiftindex_expr(x, shift) = x
-function _shiftindex_expr(x::Expr, shift)
-    if x.head == :ref
-        for i in 2:lastindex(x.args)
-            x.args[i] = :($(x.args[i]) + $shift)
+_shiftindex_in_expr(ind, shift) = ind + shift
+_shiftindex_in_expr(ind::AbstractVector, shift) = ind .+ shift
+_shiftindex_in_expr(ind, shift::Union{Expr, Symbol}) = :($ind + $shift)
+_shiftindex_in_expr(ind::Union{Expr, Symbol}, shift) = :($ind + $shift)
+
+_shiftindex_in_ref(ref, shift) = ref
+function _shiftindex_in_ref(ref::Expr, shift)
+    if ref.head == :ref
+        for i in 2:lastindex(ref.args)
+            ref.args[i] = _shiftindex_in_expr(ref.args[i], shift)
         end
-    elseif x.head == :call && x.args[1] == :getindex
-        for i in 3:lastindex(x.args)
-            x.args[i] = :($(x.args[i]) + $shift)
+    elseif ref.head == :call && ref.args[1] == :getindex
+        for i in 3:lastindex(ref.args)
+            ref.args[i] = _shiftindex_in_expr(ref.args[i], shift)
         end
     end
-    return x
+    return ref
 end
 
-_shiftindex(blk::Expr, shift) = MacroTools.postwalk(x -> _shiftindex_expr(x, shift), blk)
+_shiftindex(blk::Expr, shift) = MacroTools.postwalk(x -> _shiftindex_in_ref(x, shift), blk)
 
 """
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,19 +5,19 @@ else
     using Test
 end
 
-@testset "Unidimensional" begin
-    @test @macroexpand(@shiftindex a[1]) == :(a[1 + 1])
+@testset "Expressions/Unidimensional" begin
+    @test @macroexpand(@shiftindex a[1]) == :(a[2])
     @test @macroexpand(@shiftindex a[i + j ÷ 2]) == :(a[(i + j ÷ 2) + 1])
-    @test @macroexpand(@shiftindex begin 1 + a[1]; sin(a[x ^ 2]) end) == :(begin 1 + a[1 + 1]; sin(a[(x ^ 2) + 1]) end)
-    @test @macroexpand(@shiftindex getindex(a, 1)) == :(getindex(a, 1 + 1))
+    @test @macroexpand(@shiftindex begin 1 + a[1]; sin(a[x ^ 2]) end) == :(begin 1 + a[2]; sin(a[(x ^ 2) + 1]) end)
+    @test @macroexpand(@shiftindex getindex(a, 1)) == :(getindex(a, 2))
     @test @macroexpand(@shiftindex x a[1]) == :(a[1 + x])
 end
 
-@testset "Multidimensional" begin
-    @test @macroexpand(@shiftindex a[1, 1]) == :(a[1 + 1, 1 + 1])
+@testset "Expressions/Multidimensional" begin
+    @test @macroexpand(@shiftindex a[1, 1]) == :(a[2, 2])
     @test @macroexpand(@shiftindex a[i + j ÷ 2, i ^ 2]) == :(a[(i + j ÷ 2) + 1, (i ^ 2) + 1])
     @test @macroexpand(@shiftindex begin 1 + a[1, 2]; sin(a[x ^ 2, 1]) end) ==
-        :(begin 1 + a[1 + 1, 2 + 1]; sin(a[(x ^ 2) + 1, 1 + 1]) end)
-    @test @macroexpand(@shiftindex getindex(a, 1, 2)) == :(getindex(a, 1 + 1, 2 + 1))
+        :(begin 1 + a[2, 3]; sin(a[(x ^ 2) + 1, 2]) end)
+    @test @macroexpand(@shiftindex getindex(a, 1, 2)) == :(getindex(a, 2, 3))
     @test @macroexpand(@shiftindex x a[1, 2]) == :(a[1 + x, 2 + x])
 end
